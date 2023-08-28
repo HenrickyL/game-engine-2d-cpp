80077855486115b283c2cf9d79ad2449a312256f
HenrickyL <Henrickyl1@gmail.com>
2023-08-28 13:36:38 -0300

    return to old

--- DXUT/DXUT/WinApp.cpp
+++ DXUT/DXUT/WinApp.cpp
@@ -1,20 +1,11 @@
 #include "WinApp.h"
-#include <cmath>
-#include <string>
-#include "Engine.h"
+//#include <cmath>
+//#include <string>
+//#include "Engine.h"
 
 void WinApp::Init()
 {
-	Engine::Instance()->DisableGraphics();
-	xi = 10;
-	yi = 10;
-	xf = 100;
-	yf = 100;
-	hdc = GetDC(window->Id());
-	vxi = 1.2;
-	vyi = 0.7;
-	vxf = 0.9;
-	vyf = 1.5;
+	//Engine::Instance()->DisableGraphics();
 }
 
 // ------------------------------------------------------------------------------
@@ -24,29 +15,6 @@ void WinApp::Update()
 	// sai com o pressionamento da tecla ESC
 	if (input->KeyPress(VK_ESCAPE))
 		window->Close();
-
-	xi += vxi;
-	yi += vyi;
-	xf += vxf;
-	yf += vyf;
-
-	if (window->Height() <= yf || yf <= 0) {
-		vyf *= -1;
-	}
-
-	if (0 >= yi || yi >= window->Height()) {
-		vyi *= -1;
-
-	}
-	if (window->Width() <= xf || xf <=0) {
-		vxf *= -1;
-	}
-	if (0 >= xi || xi >= window->Width()) {
-		vxi *= -1;
-	}
-
-
-	MoveToEx(hdc, xi, yi, NULL);
 }
 
 
@@ -54,9 +22,7 @@ void WinApp::Update()
 
 void WinApp::Draw()
 {
-	window->Clear();
-	LineTo(hdc, xf, yf);
-	Sleep(5);
+
 }
 
 // ------------------------------------------------------------------------------
--- DXUT/DXUT/WinApp.h
+++ DXUT/DXUT/WinApp.h
@@ -8,14 +8,6 @@ using std::stringstream;
 class WinApp : public Game
 {
 private:
-	HDC hdc;
-	int xi, yi, xf, yf;
-	float vxi = 1;
-	float vyi = 1;
-	float vxf = 1;
-	float vyf = 1;
-	float a;
-
 	int lastX, lastY;
 
 public:
