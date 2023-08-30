a8c236a1caa0b17749a78dd9fa898ae3d26206c5
HenrickyL <Henrickyl1@gmail.com>
2023-08-30 00:31:34 -0300

    adjusts

--- DXUT/DXUT/WinMain.cpp
+++ DXUT/DXUT/WinMain.cpp
@@ -10,12 +10,14 @@ using std::stringstream;
 int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 	_In_ LPSTR lpCmdLine, _In_ int nCmdShow) {
 	try {
+		Timer t;
+		t.Start();
 		// cria motor e configura a janela
 		Engine* engine = Engine::Instance();
 		// configura a janela
 		engine->window->Mode(WINDOWED);
 		engine->window->Size(1024, 600);
-		engine->window->Color(0, 180,220);
+		engine->window->Color(0, 80,120);
 		engine->window->Title("Aplicação Windows");
 		engine->window->Icon(IDI_ICON);
 		engine->window->Cursor(IDC_CURSOR);	
@@ -24,6 +26,10 @@ int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 		engine->window->LostFocus(Engine::Pause);
 		engine->window->InFocus(Engine::Resume);
 
+		float trancorrido = t.Elapsed();
+		stringstream s;
+		s << "Transcorrido: " << trancorrido << "s\n";
+		OutputDebugString(s.str().c_str());
 		// cria e executa a aplicação
 		int exitCode = engine->Start(new WinApp());
 
