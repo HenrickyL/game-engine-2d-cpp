496e182493e6273fc070832fc7e31726addab452
HenrickyL <Henrickyl1@gmail.com>
2023-09-01 19:00:49 -0300

    Search class

---
+++ DXUT/DXUT/SearchMethods.h
@@ -0,0 +1,24 @@
+﻿#ifndef DXUT_AI_SEARCH_METHODS_H
+#define DXUT_AI_SEARCH_METHODS_H
+//------------------------------------------
+#include "INode.h"
+#include "IState.h"
+//------------------------------------------
+
+//------------------------------------------
+
+class SearchMethods {
+private:
+public:
+	/**breadth - first search
+	 * Use Queue to search result
+	 *
+	 */
+	INode* BFS(IState* initial, IState* final)
+	{
+		IState* state = initial;
+		INode* node = new INode();
+
+	}
+};
+#endif
