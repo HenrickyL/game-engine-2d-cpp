5c2eed9b045143748e448c8b39246dc125f7823e
HenrickyL <Henrickyl1@gmail.com>
2023-09-21 19:43:14 -0300

    adjust resource

--- DXUT/DXUT/Resources.h
+++ DXUT/DXUT/Resources.h
@@ -1,2 +1,7 @@
-#define IDI_ICON 101
-#define IDC_CURSOR 201
+#define IDI_ICON	101
+#define IDC_CURSOR	201
+
+//Base Image 
+#define DOT_PATH		"Resources\\default\\dot.png"
+#define CIRCLE_PATH		"Resources\\default\\circle.png"
+#define SQUARE_PATH		"Resources\\default\\square.png"
