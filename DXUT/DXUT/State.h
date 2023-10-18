e67067d3bc039240ee8ea282f23cb2805c30c8f4
HenrickyL <Henrickyl1@gmail.com>
2023-10-18 10:37:31 -0300

    refactor: add state.inl

--- DXUT/DXUT/State.h
+++ DXUT/DXUT/State.h
@@ -4,10 +4,6 @@
 #include <vector>
 #include <string>
 //------------------------------------------
-// #include "State.h"
-// #include "Transition.h"
-// #include "Action.h"
-
 template <typename T>
 class Transition;
 
@@ -24,106 +20,25 @@ protected:
     T value;
     std::vector<Transition<T>*>* edges;
 
-    void DeleteEdges(){
-        for (int i = 0; i < edges->size(); i++) {
-        Transition<T>* e = edges->at(i);
-        delete e;
-        }
-        delete edges;
-    }
+    void DeleteEdges();
 public:
-    State(){
-        edges = new std::vector<Transition<T>*>();
-        name = "default";
-    }
-    State(std::string _name){
-        edges = new std::vector<Transition<T>*>();
-        name = _name;
-    }
+    State();
+    State(std::string _name);
 
-    std::vector<Transition<T>*>* Edges() const{
-        return edges;
-    }
-    std::string Name() const{
-        return name;
-    }
-
-    void AddTransition(Transition<T>* transition){
-        edges->push_back(transition);
-    }
-
-    virtual float GetHeuristic(State<T>* target) const{
-        return 0.0f;
-    }
     virtual bool IsGeneratedPossible() const = 0;
     virtual bool Equal(State<T>* other) const = 0;
-    virtual void Generate(const std::vector<Action<T>*> actions, Dictionary<T>* controlGenerated) {
-        for (auto* action : actions) {
-            ///TODO: Validar se o estado gerado e valido
-            T key = action->Apply(this);
-            if (controlGenerated) {
-                if (!this->ExistInKeyInEdge(key) && !controlGenerated->Contains(key)) {
-                    State<T>* currentGenerated = action->Generate(this);
-                    if (currentGenerated && !this->ExistInEdge(currentGenerated)) {
-                        AddTransition(new Transition<T>(this, currentGenerated, action));
-                        if (action->Inverse() != nullptr) {
-                            AddTransition(new Transition<T>(currentGenerated, this, action));
-                        }
-                        controlGenerated->Add(key, currentGenerated);
-                    }
-                }
-                else {
-                    State<T>* inDictionary = controlGenerated->Get(key);
-                    AddTransition(new Transition<T>(this, inDictionary, action));
-                    if (action->Inverse() != nullptr) {
-                        AddTransition(new Transition<T>(inDictionary, this, action));
-                    }
-                }
-            }
-            else {
-                State<T>* currentGenerated = action->Generate(this);
-                // Verifique se o novo estado já existe na lista de estados ou se é ambíguo
-                if (currentGenerated && !this->ExistInEdge(currentGenerated)) {
-                    AddTransition(new Transition<T>(this, currentGenerated, action));
-                }
-                else {
-                    delete currentGenerated; // Descarte o estado ambíguo
-                }
-            }
-        }
-    
-    }
-    //virtual bool IsAmbiguous(State<T>* newState) const = 0;
+    virtual void Generate(const std::vector<Action<T>*> actions, Dictionary<T>* controlGenerated);
 
-    bool ExistActionInEdge(Action<T>* action){
-        for (Transition<T>* t : *edges) {
-            if (t->GetAction() == action) {
-                return true;
-            }
-        }
-        return false;
-    }
-    bool ExistInEdge(State<T>* _target) const{
-        for (const Transition<T>* t : *edges) {
-            if (t->GetTarget()->Equal(_target)) {
-                return true;
-            }
-        }
-        return false;
-    }
-    bool ExistInKeyInEdge(T _key) const {
-        for (const Transition<T>* t : *edges) {
-            if (t->GetTarget()->Value() == _key) {
-                return true;
-            }
-        }
-        return false;
-    }
+    void AddTransition(Transition<T>* transition);
 
+    std::string Name() const;
+    std::vector<Transition<T>*>* Edges() const;
+    virtual float GetHeuristic(State<T>* target) const;
+    bool ExistActionInEdge(Action<T>* action) const;
+    bool ExistInEdge(State<T>* _target) const;
+    bool ExistInKeyInEdge(T _key) const;
     ///TODO: validate whether the name 'Value' is the best, as it will be used as 'Key' later.
-    T Value()const {
-        return value;
-    }
+    T Value()const;
 };
-
+#include "State.inl"
 #endif
---
+++ DXUT/DXUT/State.inl
@@ -0,0 +1,120 @@
+//#include "State.h"
+
+#include <vector>
+#include <string>
+#include "Transition.h"
+#include "Action.h"
+
+template<typename T>
+State<T>::State() {
+    edges = new std::vector<Transition<T>*>();
+    name = "default";
+}
+
+template <typename T>
+State<T>::State(std::string _name) {
+    edges = new std::vector<Transition<T>*>();
+    name = _name;
+}
+
+template <typename T>
+void State<T>::DeleteEdges() {
+    for (int i = 0; i < edges->size(); i++) {
+        Transition<T>* e = edges->at(i);
+        delete e;
+    }
+    delete edges;
+}
+
+template <typename T>
+std::vector<Transition<T>*>* State<T>::Edges() const {
+    return edges;
+}
+
+template <typename T>
+std::string State<T>::Name() const {
+    return name;
+}
+
+template <typename T>
+void State<T>::AddTransition(Transition<T>* transition) {
+    edges->push_back(transition);
+}
+
+template <typename T>
+float State<T>::GetHeuristic(State<T>* target) const {
+    return 0.0f;
+}
+
+template <typename T>
+bool State<T>::ExistActionInEdge(Action<T>* action) const {
+    for (Transition<T>* t : *edges) {
+        if (t->GetAction() == action) {
+            return true;
+        }
+    }
+    return false;
+}
+
+template <typename T>
+bool State<T>::ExistInEdge(State<T>* _target) const {
+    for (const Transition<T>* t : *edges) {
+        if (t->GetTarget()->Equal(_target)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+template <typename T>
+bool State<T>::ExistInKeyInEdge(T _key) const {
+    for (const Transition<T>* t : *edges) {
+        if (t->GetTarget()->Value() == _key) {
+            return true;
+        }
+    }
+    return false;
+}
+
+template <typename T>
+T State<T>::Value()const {
+    return value;
+}
+
+template <typename T>
+void State<T>::Generate(const std::vector<Action<T>*> actions, Dictionary<T>* controlGenerated) {
+    for (auto* action : actions) {
+        ///TODO: Validar se o estado gerado e valido
+        T key = action->Apply(this);
+        if (controlGenerated) {
+            if (!this->ExistInKeyInEdge(key) && !controlGenerated->Contains(key)) {
+                State<T>* currentGenerated = action->Generate(this);
+                if (currentGenerated && !this->ExistInEdge(currentGenerated)) {
+                    AddTransition(new Transition<T>(this, currentGenerated, action));
+                    if (action->Inverse() != nullptr) {
+                        AddTransition(new Transition<T>(currentGenerated, this, action));
+                    }
+                    controlGenerated->Add(key, currentGenerated);
+                }
+            }
+            else {
+                State<T>* inDictionary = controlGenerated->Get(key);
+                AddTransition(new Transition<T>(this, inDictionary, action));
+                if (action->Inverse() != nullptr) {
+                    AddTransition(new Transition<T>(inDictionary, this, action));
+                }
+            }
+        }
+        else {
+            State<T>* currentGenerated = action->Generate(this);
+            // Verifique se o novo estado já existe na lista de estados ou se é ambíguo
+            if (currentGenerated && !this->ExistInEdge(currentGenerated)) {
+                AddTransition(new Transition<T>(this, currentGenerated, action));
+            }
+            else {
+                delete currentGenerated; // Descarte o estado ambíguo
+            }
+        }
+    }
+}
+
