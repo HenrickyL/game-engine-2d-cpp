86f1f3e9f0d2daeed829bef5db98a393333d392a
HenrickyL <Henrickyl1@gmail.com>
2023-11-10 15:26:47 -0300

    adjust

--- DXUT/DXUT/WinMain.cpp
+++ DXUT/DXUT/WinMain.cpp
@@ -1,6 +1,5 @@
 #include "DXUT.h"
-//#include "MissionariesCannibalsProblem.h"
-#include "ShortPathProblem.h"
+#include "MissionariesCannibalsProblem.h"
 
 #include <sstream>
 
@@ -29,7 +28,7 @@ int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 		engine->window->LostFocus(Engine::Pause);
 		engine->window->InFocus(Engine::Resume);
 		// cria e executa a aplicação
-		int exitCode = engine->Start(new ShortPathProblem());
+		int exitCode = engine->Start(new MissionariesCannibalsProblem());
 
 		// finaliza execução
 		delete engine;
