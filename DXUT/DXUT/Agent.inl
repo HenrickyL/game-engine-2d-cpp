faf8889e3b4297a72b997aefea198f946cff6d37
HenrickyL <HenrickyL1@gmail.com>
2023-11-17 15:20:45 -0300

    refactor & adjust

--- DXUT/DXUT/Agent.inl
+++ DXUT/DXUT/Agent.inl
@@ -56,13 +56,12 @@ void Agent<T>::Search(GenerateType generateType) {
 	///TODO: Clear - DEBUG
 	std::string res = path != nullptr? path->GetPath() : "NotFound";
 	pathLength = path != nullptr ? path->GetPathLength() : 0;
-	std::string pathFile ="C:\\Users\\henri\\OneDrive\\Área de Trabalho\\GAME_Search_Result\\solver"+ _finalState->Name() + ".txt";
-	FileHandler writer(pathFile);
+	FileHandler writer;
 	writer.Append("#" + _finalState->Name());
 	writer.Append("time: " + std::to_string(timer) + "s");
 	writer.Append("steps: " + std::to_string(pathLength) + "\n-------------");
 	writer.Append(res);
-	writer.Save();
+	writer.Save("solver"+_finalState->Name() + ".txt");
 }
 
 template<typename T>
--- DXUT/DXUT/FileHandler.cpp
+++ DXUT/DXUT/FileHandler.cpp
@@ -1,18 +1,35 @@
 #include "FileHandler.h"
 #include <fstream>
 #include <iostream> // Para output de erros
+#include <filesystem>
 
-FileHandler::FileHandler(const std::string& path) : filePath(path) {
+FileHandler::FileHandler(const std::string& path) : path(path) {
+    // Construtor, inicializa o caminho do arquivo
+    SetUsername();
+}
+
+FileHandler::FileHandler() {
+    SetUsername();
+    path = "C:\\Users\\"+username+"\\Documents\\GAME_Search_Result\\";
     // Construtor, inicializa o caminho do arquivo
 }
 
 FileHandler::~FileHandler() {
     // Destrutor
-    Save(); // Salva as alterações no arquivo antes de destruir a instância
+    if(!saved)
+        Save(); // Salva as alterações no arquivo antes de destruir a instância
+}
+
+
+void FileHandler::SetUsername() {
+    char userName[MAX_PATH];
+    DWORD usernameSize = sizeof(userName);
+    GetUserNameA(userName, &usernameSize);
+    username = std::string(userName);
 }
 
 bool FileHandler::Read() {
-    std::ifstream fileStream(filePath);
+    std::ifstream fileStream(path);
 
     if (!fileStream.is_open()) {
         return false; // Falha ao abrir o arquivo
@@ -28,19 +45,31 @@ bool FileHandler::Read() {
 }
 
 void FileHandler::Write(const std::string& data) {
+    saved = false;
     fileLines.clear(); // Limpa as linhas existentes
     fileLines.push_back(data); // Adiciona a nova linha
 }
 
 void FileHandler::Append(const std::string& data) {
+    saved = false;
     fileLines.push_back(data); // Adiciona a nova linha ao final do vetor
 }
 
-void FileHandler::Save() {
-    std::ofstream fileStream(filePath);
+void FileHandler::Save(std::string fileName) {
+    std::string pathF = path + fileName;
+    std::ofstream fileStream(pathF);
+
+    // Cria o diretório se não existir
+    std::filesystem::path directory = std::filesystem::path(path);
+    if (!std::filesystem::exists(directory)) {
+        if (!std::filesystem::create_directories(directory)) {
+            std::cerr << "Failed to create directory: " << path << std::endl;
+            return;
+        }
+    }
 
     if (!fileStream.is_open()) {
-        std::cerr << "Failed to open file for writing: " << filePath << std::endl;
+        std::cerr << "Failed to open file for writing: " << pathF << std::endl;
         return;
     }
 
@@ -49,6 +78,7 @@ void FileHandler::Save() {
     }
 
     fileStream.close();
+    saved = true;
 }
 
 const std::vector<std::string>& FileHandler::GetLines() const {
