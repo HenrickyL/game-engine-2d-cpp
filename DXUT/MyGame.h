3e84e330a442076ba206c7dc2cc8f531c4caa0ad
HenrickyL <HenrickyL1@gmail.com>
2024-04-22 18:28:12 -0300

    create GameTest

---
+++ DXUT/MyGame.h
@@ -0,0 +1,43 @@
+#ifndef MYCLASS_TEST_H
+#define MYCLASS_TEST_H
+
+
+#include "Game.h"
+#include "Geometry.h"
+#include "GLDrawGeometry.h"
+#include "GLCamera.h"
+#include <string.h>
+#include <sstream>
+
+using std::stringstream;
+
+struct Obj {
+	int x = 0;
+	int y = 0;
+	float z = -25;
+};
+
+
+class MyGame : public Game {
+private:
+	Geometry* obj;
+	Rect* rect;
+	GLDrawGeometry drawner;
+	GLCamera* cam;
+	string s = "";
+	float globalRotation = 0;
+	float delta = 0.1;
+	std::vector<void (*)(Obj)> functionVector;
+	int index = 0;
+	Movable* object;
+
+	Window* window;
+
+public:
+	void Init() override;
+	void Update() override;
+	void Draw() override;
+	void Finalize() override;
+};
+
+#endif
