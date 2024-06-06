18b6a859ca97a584789fc987e2383686df61926e
HenrickyL <HenrickyL1@gmail.com>
2024-06-06 03:05:29 -0300

    adjust error in last commit

--- DXUT/GLRenderer3D.cpp
+++ DXUT/GLRenderer3D.cpp
@@ -15,11 +15,60 @@ void GLRenderer3D::Draw(const Shape3D& shape) {
 
 
 
-class Renderer3D : public RendererBase {
-private:
-    virtual void DrawShape(const Shape3D& shape) const = 0;
+void GLRenderer3D::DrawShape(const Shape3D& shape) const {
+    vector<Vertex> vertices = shape.vertices();
+    vector<Triangle> triangles = shape.triangles();
 
-public:
-    virtual void Draw(const Shape3D& shape) = 0;
-};
+    Color c = shape.color();
+
+    if (shape.isFlatColor()) {
+        glColor3f(c.r(), c.g(), c.b());
+    }
+
+    if (_fillMode == F_SOLID || _fillMode == F_WIREFRAME_SOLID) {
+        for (const Triangle& triangle : triangles) {
+            glBegin(GL_TRIANGLES);
+            // Loop through each vertex in the triangle and draw it
+            for (int i = 0; i < 3; ++i) {
+                const Vertex& vertex = triangle.vertices[i];
+                glColor3f(vertex.r(), vertex.g(), vertex.b());
+                glVertex3f(vertex.position.x(), vertex.position.y(), vertex.position.z());
+            }
+            glEnd();
+        }
+    }
+
+    if (_fillMode == F_WIREFRAME || _fillMode == F_WIREFRAME_SOLID) {
+        glColor3f(c.r(), c.g(), c.b());
+        glLineWidth(1.2f);
+        for (const Triangle& triangle : triangles) {
+            glBegin(GL_LINE_LOOP);
+            for (int i = 0; i < 3; ++i) {
+                const Vertex& vertex = triangle.vertices[i];
+                Color lineColor = vertex.color;
+                if (_fillMode == F_WIREFRAME_SOLID) {
+                    lineColor = vertex.color.Brightness(0.2);
+                }
+                glColor3f(lineColor.r(), lineColor.g(), lineColor.b());
+                glVertex3f(vertex.position.x(), vertex.position.y(), vertex.position.z());
+            }
+            glEnd();
+        }
+    }
+
+    if (_fillMode == F_POINTS) {
+        glColor3f(c.r(), c.g(), c.b());
+        glPointSize(2.5f);
+        glBegin(GL_POINTS);
+        for (int i = 0; i < vertices.size(); i++) {
+            Vertex v = vertices[i];
+            if (!shape.isFlatColor()) {
+                glColor3f(v.r(), v.g(), v.b());
+            }
+            glVertex3f(v.x(), v.y(), v.z());
+        }
+        glEnd();
+    }
+}
+
 
