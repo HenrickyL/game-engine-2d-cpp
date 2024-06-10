8a241f9db8ef1f2a196abf655d6a60b4b7aba622
HenrickyL <HenrickyL1@gmail.com>
2024-06-09 23:40:12 -0300

    add glm & other Files VertexBufferID

--- DXUT/Instance.h
+++
@@ -1,8 +0,0 @@
-﻿#ifndef DXUT_UTILS_INSTANCE_OF_H
-#define DXUT_UTILS_INSTANCE_OF_H
-
-template<typename Base, typename T>
-inline bool instanceOf(const T* ptr) {
-	return dynamic_cast<const Base*>(ptr) != nullptr;
-}
-#endif
