3eaf5628ff7ae897ce82a7e26bfcc67f8074d3fa
HenrickyL <HenrickyL1@gmail.com>
2024-07-06 13:07:21 -0300

    change

--- DXUT/WinMain.cpp
+++ DXUT/WinMain.cpp
@@ -13,7 +13,7 @@ int UseEngine(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 	try {
 		// cria motor e configura a janela
 		Engine* engine = Engine::Instance();
-		engine->SetType(T_2D);
+		engine->SetType(T_3D);
 		// configura a janela
 		engine->window->Mode(WINDOWED);
 
