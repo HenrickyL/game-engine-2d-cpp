1ca314dd4fa8ad1a8ed567d082fa34a106889c6c
HenrickyL <HenrickyL1@gmail.com>
2024-07-16 11:43:44 -0300

    add new Game To Test 2d context

---
+++ DXUT/Test2d.cpp
@@ -0,0 +1,36 @@
+#include "Test2d.h"
+
+
+
+
+
+void Test2D::Reset() {
+}
+
+
+void Test2D::Init() {
+
+	geo = new Rect(Position(0,0,0), 0.5,1, Color::RED);
+	geometries.push_back(geo);
+	window->SetColor(Color::GRAY);
+}
+void Test2D::Update(double frameTime) {
+	glLoadIdentity();
+	glTranslatef(0, 0, 0);
+	gluLookAt(0.0, 0.0, 5.0,  // Posição da câmera (eye)
+		0.0, 0.0, 0.0, // Ponto para onde a câmera está olhando (center)
+		0.0, 1.0, 0.0); // Vetor "up" que define a orientação da câmera (up)
+}
+
+
+void Test2D::Draw() {
+	for (Geometry* g : geometries) {
+		_drawnner.Draw(*g);
+	}
+	
+}
+void Test2D::Finalize() {
+	for (Geometry* g : geometries) {
+		delete g;
+	}
+}
